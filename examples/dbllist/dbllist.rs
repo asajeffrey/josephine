d358e42ff251403e59e06a32ef002e76550166e0
Alan Jeffrey <ajeffrey@mozilla.com>
2017-10-20 11:51:55 -0500

    Added doubly linked list example.

---
+++ examples/dbllist/dbllist.rs
@@ -0,0 +1,111 @@
+//! A simple doubly linked list class.
+
+use linjs::CanAccess;
+use linjs::CanAlloc;
+use linjs::Compartment;
+use linjs::InCompartment;
+use linjs::Initialized;
+use linjs::IsInitialized;
+use linjs::IsInitializing;
+use linjs::JSContext;
+use linjs::JSManaged;
+use linjs::JSRootable;
+
+#[derive(Copy, Clone, JSTraceable, JSRootable, JSTransplantable)]
+pub struct DoublyLinkedList<'a, C>(JSManaged<'a, C, NativeDoublyLinkedList<'a, C>>);
+
+#[derive(JSTraceable, JSRootable, JSInitializable, JSTransplantable)]
+pub struct NativeDoublyLinkedList<'a, C> {
+    first: Option<Cell<'a, C>>,
+    last: Option<Cell<'a, C>>,
+}
+
+#[derive(Copy, Clone, JSTraceable, JSRootable, JSTransplantable)]
+pub struct Cell<'a, C>(JSManaged<'a, C, NativeCell<'a, C>>);
+
+#[derive(JSTraceable, JSRootable, JSInitializable, JSTransplantable)]
+pub struct NativeCell<'a, C> {
+    data: String,
+    prev: Option<Cell<'a, C>>,
+    next: Option<Cell<'a, C>>,
+}
+
+impl<'a, C:'a> DoublyLinkedList<'a, C> {
+    pub fn init<S>(cx: JSContext<S>) -> JSContext<Initialized<'a, C, NativeDoublyLinkedList<'a, C>>> where
+        S: IsInitializing<'a, C, NativeDoublyLinkedList<'a, C>>,
+    {
+        cx.global_manage(NativeDoublyLinkedList { first: None, last: None })
+    }
+
+    pub fn global<S>(cx: &JSContext<S>) -> DoublyLinkedList<'a, C> where
+        S: IsInitialized<'a, C, NativeDoublyLinkedList<'a, C>>,
+    {
+        DoublyLinkedList(cx.global())
+    }
+}
+
+impl<'a, C:'a> DoublyLinkedList<'a, C> {
+    pub fn first<S>(self, cx: &'a JSContext<S>) -> Option<Cell<'a, C>> where
+        S: CanAccess,
+        C: Compartment,
+    {
+        self.0.borrow(cx).first
+    }
+
+    pub fn last<S>(self, cx: &'a JSContext<S>) -> Option<Cell<'a, C>> where
+        S: CanAccess,
+        C: Compartment,
+    {
+        self.0.borrow(cx).last
+    }
+
+    pub fn push<S>(self, cx: &mut JSContext<S>, data: String) where
+        S: CanAccess + CanAlloc + InCompartment<C>,
+        C: Compartment,
+    {
+        let ref mut root1 = cx.new_root();
+        let ref mut root2 = cx.new_root();
+        let old_last = self.last(cx).in_root(root1);
+        let new_last = Cell::new(cx, data, old_last, None).in_root(root2);
+        self.0.borrow_mut(cx).last = Some(new_last);
+        if let Some(old_last) = old_last {
+            old_last.0.borrow_mut(cx).next = Some(new_last);
+        } else {
+            self.0.borrow_mut(cx).first = Some(new_last);
+        }
+    }
+}
+
+impl<'a, C:'a> Cell<'a, C> {
+    fn new<S>(cx: &'a mut JSContext<S>, data: String, prev: Option<Cell<'a, C>>, next: Option<Cell<'a, C>>) -> Cell<'a, C> where
+        S: CanAlloc + InCompartment<C>,
+        C: Compartment,
+    {
+        Cell(cx.manage(NativeCell {
+            data: data,
+            prev: prev,
+            next: next,
+        }))
+    }
+
+    pub fn data<S>(self, cx: &'a JSContext<S>) -> &'a str where
+        S: CanAccess,
+        C: Compartment,
+    {
+        &*self.0.borrow(cx).data
+    }
+
+    pub fn prev<S>(self, cx: &'a JSContext<S>) -> Option<Cell<'a, C>> where
+        S: CanAccess,
+        C: Compartment,
+    {
+        self.0.borrow(cx).prev
+    }
+
+    pub fn next<S>(self, cx: &'a JSContext<S>) -> Option<Cell<'a, C>> where
+        S: CanAccess,
+        C: Compartment,
+    {
+        self.0.borrow(cx).next
+    }
+}
---
+++ examples/dbllist/main.rs
@@ -0,0 +1,29 @@
+extern crate linjs;
+#[macro_use] extern crate linjs_derive;
+
+mod dbllist;
+
+use dbllist::DoublyLinkedList;
+
+use linjs::JSContext;
+
+fn main() {
+    // Create a new JS context
+    let ref mut cx = JSContext::new();
+
+    // Create a new doubly-linked list in its own compartment
+    let ref mut cx = DoublyLinkedList::init(cx.create_compartment());
+
+    // Get the list from the newly created compartment
+    let list = DoublyLinkedList::global(cx);
+
+    // Push some data onto the list
+    list.push(cx, String::from("hello"));
+    list.push(cx, String::from("world"));
+
+    // Check that the list has the data we expect
+    assert_eq!(list.first(cx).unwrap().data(cx), "hello");
+    assert_eq!(list.first(cx).unwrap().next(cx).unwrap().data(cx), "world");
+    assert_eq!(list.last(cx).unwrap().data(cx), "world");
+    assert_eq!(list.last(cx).unwrap().prev(cx).unwrap().data(cx), "hello");
+}
