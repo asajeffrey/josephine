c681da74837b989ee1cedae3134540b9894b3222
Alan Jeffrey <ajeffrey@mozilla.com>
2017-10-23 10:45:35 -0500

    Started updating rustdoc documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,40 @@
-//! An outline of how Rust's ownership, borrowing and lifetimes could be combined with JS-managed data
+//! A library which uses JavaScript to safely manage the lifetimes of Rust data.
+//!
+//! This library allows Rust data to be attached to JavaScript objects:
+//! the lifetime of the Rust data is then the same as the JS object it is attached to.
+//! Since JS is garbage collected, it is safe to copy and discard references to
+//! JS managed data, using an API similar to the `Rc` API, but which does not need
+//! dynamic checks since those are handled by the garbage collector. This makes it possible
+//! to implement structures like doubly-linked lists without dynamic checking.
 //!
 //! The goals are:
 //!
-//! 1. Ensure that JS objects are only accessed in the right JS compartment.
-//! 2. Support stack-allocated roots.
-//! 3. Remove the need for the rooting lint.
-//! 4. Don't require rooting in code that can't perform GC.
-//! 5. Allow `&mut T` access to JS-managed data, so we don't need as much interior mutability.
+//! 1. Use JS to manage the lifetime of Rust data.
+//! 2. Allow references JS managed data to be freely copied and discarded, relying on
+//!    the garbage collector for safety.
+//! 3. Maintain Rust memory safety (for example no mutable aliasing),
+//!    without requiring additional static analysis such as a lint.
+//! 4. Allow mutable and immutable access to Rust data via JS managed references, so
+//!    we do not need to rely on interior mutability.
+//! 5. Provide a rooting API to ensure that JS managed data is not garbage collected
+//!    while it is being used.
+//!
+//! To support safe access to JS managed data, the API uses a *JS context*, which
+//! is used as an access token to allow JS managed data to be accessed, allocated
+//! or deallocated. Mutable access to JS managed data requires mutable access to the
+//! JS context, which is how the API achieves memory safety even though JS managed
+//! references can be copied and discarded freely.
+//!
+//! JS managed memory is split into *compartments*, which are
+//! separately garbage collected, so the garbage collector can avoid
+//! scanning the entire heap. The API statically tracks compartments, to
+//! ensure that there are no direct references between compartments.
+//!
+//! The API is implemented as bindings to the SpiderMonkey JS engine,
+//! from which it borrows the garbage collector and the notions of compartment
+//! and JS context. The API allows calling into JavaScript
+//! from Rust, and calling back into Rust from JavaScript. These bindings are unsafe,
+//! and are intended for use by a trusted bindings generator.
 //!
 //! # JS-managed data
 //!
